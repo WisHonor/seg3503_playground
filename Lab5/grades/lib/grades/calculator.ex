b326eb438fdf5c3641af709aa21430d37d9a928f
Thagg3rs <alex.turgeon@icloud.com>
2026-07-03 15:03:34 -0400

    Add grades calculator stub

---
+++ Lab5/grades/lib/grades/calculator.ex
@@ -0,0 +1,6 @@
+defmodule Grades.Calculator do
+  # STUB - temporary constant values, ignores the grades map.
+  def letter_grade(_grades), do: "A+"
+  def numeric_grade(_grades), do: 10
+  def percentage_grade(_grades), do: 95.0
+end
